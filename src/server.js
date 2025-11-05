51e9d5767b39073dacf8d7b8e73a67dc946b5241
Claudinei <claudineimoreirax7l@gmail.com>
2025-11-04 21:05:55 -0300

    feat(server): Permitindo cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import usersRouter from "./routes/users.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // URL do front
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(bodyParser.json());
+
+// Rotas
 app.use("/users", usersRouter);
 
-const PORT = process.env.PORT || 3000;
+// Inicializa o servidor
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
