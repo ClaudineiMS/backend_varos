081e4940844111112bdff183901def575d9ebafa
Claudinei <claudineimoreirax7l@gmail.com>
2025-11-04 21:38:05 -0300

    fix(cors): Ajuste do cors

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // URL do front
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
